2caa50307ee634bfc87a30041b98fcef3f475353
SatriaBagus <ijusciti@gmail.com>
2021-11-20 11:23:27 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// pada saat halaman di refresh
+
+$(window).on('load', function () {
+    $('.trans h5').addClass('pmuncul');
+    $('.trans p').addClass('pmuncul');
+    $('.konten').addClass('pmuncul');
+
+});
